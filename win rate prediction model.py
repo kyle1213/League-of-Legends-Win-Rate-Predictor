bec7ec276a5cf02fb255b243a2204bc0f0721854
kyle1213 <jayk01213@naver.com>
2021-04-07 03:56:22 +0900

    added new learnable parameters using torch.nn.Parameter, and changed model

--- win rate prediction model.py
+++ win rate prediction model.py
@@ -6,31 +6,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-with open('.\data\output2.json') as json_file:
-    json_data = json.load(json_file)
-    json_array = []
-    print(len(json_data))
+with open('./data/output2.json') as json_train_file:
+    json_train_data = json.load(json_train_file)
+    json_train_array = []
+    print(len(json_train_data))
 
-    for i in range(len(json_data)):
-        json_array.append(json_data[i]["array"])
+    for i in range(len(json_train_data)):
+        json_train_array.append(json_train_data[i]["array"])
 
 x_train = []
 y_train = []
 
-for i in range(len(json_data)):
-    x_train.append(json_array[i][:10])
-    y_train.append(json_array[i][10])
+for i in range(len(json_train_data)):
+    x_train.append(json_train_array[i][:10])
+    y_train.append(json_train_array[i][10])
 
 x_train = np.array(x_train)
+x_train = np.reshape(x_train, [40180, 2, 5])
 x_train = x_train / 152
 
-for i in range(len(json_data)):
+for i in range(len(json_train_data)):
     if y_train[i] == 0:
         y_train[i] = [1, 0]
     if y_train[i] == 1:
         y_train[i] = [0, 1]
 
 y_train = np.array(y_train)
+
+with open('./data/test.json') as json_test_file:
+    json_test_data = json.load(json_test_file)
+    json_test_array = []
+    print(len(json_test_data))
+
+    for i in range(len(json_test_data)):
+        json_test_array.append(json_test_data[i]["array"])
+
+x_test = []
+y_test = []
+
+for i in range(len(json_test_data)):
+    x_test.append(json_test_array[i][:10])
+    y_test.append(json_test_array[i][10])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, [75, 2, 5])
+x_test = x_test / 152
+
+for i in range(len(json_test_data)):
+    if y_test[i] == 0:
+        y_test[i] = [1, 0]
+    if y_test[i] == 1:
+        y_test[i] = [0, 1]
+
+y_test = np.array(y_test)
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -47,7 +75,8 @@ class Dataset(torch.utils.data.Dataset):
         return len(self.features)
 
 
-train_loader = torch.utils.data.DataLoader(dataset=Dataset(x_train, y_train), batch_size=1000, shuffle=False)
+train_loader = torch.utils.data.DataLoader(dataset=Dataset(x_train, y_train), batch_size=1000, shuffle=True)
+test_loader = torch.utils.data.DataLoader(dataset=Dataset(x_test, y_test), batch_size=1000, shuffle=False)
 
 
 cuda = torch.device('cuda')
@@ -56,7 +85,10 @@ cuda = torch.device('cuda')
 class LOL(nn.Module):
     def __init__(self):
         super(LOL, self).__init__()
-        self.layer1 = nn.Sequential(nn.Linear(10, 128),
+        self.matrix_weight_1 = torch.nn.Parameter(torch.randn(5, 1).cuda())
+        self.matrix_weight_2 = torch.nn.Parameter(torch.randn(2, 1).cuda())
+        #use torch.bmm
+        self.layer1 = nn.Sequential(nn.Linear(17, 128),
                                     nn.ReLU(),
                                     nn.Dropout(),
                                     nn.BatchNorm1d(128),
@@ -87,6 +119,13 @@ class LOL(nn.Module):
                                     nn.Linear(256, 2))
 
     def forward(self, x):
+        reshaped_x_1 = torch.matmul(x, self.matrix_weight_1)
+        tmp_x = torch.transpose(x, 2, 1)
+        reshaped_x_2 = torch.matmul(tmp_x, self.matrix_weight_2)
+        x = torch.reshape(x, [-1, 10])
+        reshaped_x_1 = torch.squeeze(reshaped_x_1)
+        reshaped_x_2 = torch.squeeze(reshaped_x_2)
+        x = torch.cat((x, reshaped_x_1, reshaped_x_2), dim=1)
         x = self.layer1(x)
         return x
 
@@ -94,10 +133,9 @@ class LOL(nn.Module):
 model = LOL()
 model = model.cuda()
 
-#loss = nn.MSELoss(reduction='mean')
 loss = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001, weight_decay=5e-4, momentum=0.9)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1000, gamma=0.5)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5000, gamma=0.5)
 cost = 0
 
 iterations = []
@@ -105,7 +143,7 @@ train_losses = []
 train_acc = []
 
 
-for epoch in range(100):
+for epoch in range(1000):
     model.train()
     correct = 0
     for x, y in train_loader:
@@ -128,12 +166,26 @@ for epoch in range(100):
     train_losses.append(cost.tolist())
     train_acc.append((100 * correct / len(train_loader.dataset)).tolist())
 
+model.eval()
+correct = 0
+for data, target in test_loader:
+    data = data.float()
+    target = target.float()
+    data = data.to(cuda)
+    target = target.to(cuda)
+    output = model(data)
+    prediction = output.data.max(1)[1]
+    correct += prediction.eq(target.data.max(1)[1]).sum()
+
+print('Test set: Accuracy: {:.2f}%'.format(100. * correct / len(test_loader.dataset)))
+
 plt.subplot(121)
 plt.plot(range(1, len(iterations)+1), train_losses, 'b--')
 plt.subplot(122)
 plt.plot(range(1, len(iterations)+1), train_acc, 'b-')
 plt.title('loss and accuracy')
 plt.show()
+
 
 """
 from keras.models import Sequential
